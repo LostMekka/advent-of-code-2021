53d02be9fbb89b3ec600458de8948f8c05ca33e5
LostMekka <lostmekka@users.noreply.github.com>
2021-12-10 13:46:39 +0100

    added day template for convenience

---
+++ src/day_template/DayXX.kt
@@ -0,0 +1,23 @@
+package day_template
+
+import util.readInput
+import util.shouldBe
+
+fun main() {
+    val day = 0 // TODO
+    val testInput = readInput(day, testInput = true)
+    part1(testInput) shouldBe 1 // TODO
+    part2(testInput) shouldBe 1 // TODO
+
+    val input = readInput(day)
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun part1(input: List<String>): Int {
+    return 1 // TODO
+}
+
+private fun part2(input: List<String>): Int {
+    return 1 // TODO
+}
